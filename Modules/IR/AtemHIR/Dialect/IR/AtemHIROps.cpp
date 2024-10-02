8c83dccc9249c3b36256522726caa93b687eb402
Zhige Chen <zhigec_cpp@outlook.com>
2024-10-02 11:51:15 +0800

    more more ops types and attrs

--- Modules/IR/AtemHIR/Dialect/IR/AtemHIROps.cpp
+++ Modules/IR/AtemHIR/Dialect/IR/AtemHIROps.cpp
@@ -1,6 +1,11 @@
 #include "IR/AtemHIR/Dialect/IR/AtemHIROps.hpp"
 #include "IR/AtemHIR/Dialect/IR/AtemHIRDialect.hpp"
 
+auto ensureRegionTerminator(mlir::OpAsmParser &parser, mlir::Region &region, llvm::SMLoc error_loc) -> llvm::LogicalResult;
+auto canOmitRegionTerminator(mlir::Region &region) -> bool;
+static auto parseOmittedTerminatorRegion(mlir::OpAsmParser &parser, mlir::Region &region) -> mlir::ParseResult;
+static auto printOmittedTerminatorRegion(mlir::OpAsmPrinter &printer, mlir::atemhir::ScopeOp &op, mlir::Region &region) -> void;
+
 #define GET_OP_CLASSES
 #include "Modules/IR/AtemHIR/Dialect/IR/AtemHIR.cpp.inc"
 
@@ -19,7 +24,60 @@ auto mlir::atemhir::AtemHIRDialect::registerOperations() -> void
 
 auto mlir::atemhir::buildTerminatedBody(OpBuilder &builder, Location loc) -> void
 {
+}
 
+auto ensureRegionTerminator(mlir::OpAsmParser &parser, mlir::Region &region, llvm::SMLoc error_loc) -> llvm::LogicalResult
+{
+    using namespace mlir;
+    Location error_location = parser.getEncodedSourceLoc(parser.getCurrentLocation());
+    OpBuilder builder(parser.getBuilder().getContext());
+
+    if (region.empty() or (region.back().mightHaveTerminator() and region.back().getTerminator()))
+    {
+        return success();
+    }
+
+    if (not region.hasOneBlock())
+    {
+        return parser.emitError(error_loc, "multi-block region must not omit terminator");
+    }
+
+    if (region.back().empty())
+    {
+        return parser.emitError(error_loc, "empty region must not omit terminator");
+    }
+
+    region.back().push_back(builder.create<atemhir::YieldOp>(error_location));
+    return success();
+}
+
+auto canOmitRegionTerminator(mlir::Region &region) -> bool
+{
+    auto const single_non_empty_block = region.hasOneBlock() and not region.back().empty();
+    auto const yields_nothing = [&region]() {
+        mlir::atemhir::YieldOp yield_op = mlir::dyn_cast<mlir::atemhir::YieldOp>(region.back().getTerminator());
+        return yield_op and yield_op.getArgs().empty();
+    }();
+    return single_non_empty_block and yields_nothing;
+}
+
+static auto parseOmittedTerminatorRegion(mlir::OpAsmParser &parser, mlir::Region &region) -> mlir::ParseResult
+{
+    auto region_loc = parser.getCurrentLocation();
+    if (parser.parseRegion(region))
+    {
+        return mlir::failure();
+    }
+    if (ensureRegionTerminator(parser, region, region_loc).failed())
+    {
+        return mlir::failure();
+    }
+    return mlir::success();
+}
+
+static auto printOmittedTerminatorRegion(mlir::OpAsmPrinter &printer, mlir::atemhir::ScopeOp &op, mlir::Region &region) -> void
+{
+    printer.printRegion(region, false, not canOmitRegionTerminator(region));
 }
 
 //========================================================
@@ -104,24 +162,35 @@ auto mlir::atemhir::AllocateVarOp::handlePromotionComplete(MemorySlot const &slo
 
 auto mlir::atemhir::LoadOp::loadsFrom(MemorySlot const &slot) -> bool
 {
+    return this->getAddr() == slot.ptr;
 }
 
 auto mlir::atemhir::LoadOp::storesTo(MemorySlot const &slot) -> bool
 {
+    return false;
 }
 
 auto mlir::atemhir::LoadOp::getStored(MemorySlot const &slot, OpBuilder &builder, Value reaching_def, DataLayout const &data_layout) -> Value
 {
+    llvm_unreachable("getStored() shouldn't be called on atemhir::LoadOp");
 }
 
 auto mlir::atemhir::LoadOp::canUsesBeRemoved(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses,
                                              SmallVectorImpl<OpOperand *> &new_blocking_uses, DataLayout const &data_layout) -> bool
 {
+    if (blocking_uses.size() != 1)
+    {
+        return false;
+    }
+    Value const blocking_use = (*blocking_uses.begin())->get();
+    return blocking_use == slot.ptr and this->getAddr() == slot.ptr and this->getResult().getType() == slot.elemType;
 }
 
 auto mlir::atemhir::LoadOp::removeBlockingUses(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses, OpBuilder &builder,
                                                Value reaching_def, DataLayout const &data_layout) -> DeletionKind
 {
+    this->getResult().replaceAllUsesWith(reaching_def);
+    return DeletionKind::Delete;
 }
 
 //========================================================
@@ -130,24 +199,34 @@ auto mlir::atemhir::LoadOp::removeBlockingUses(MemorySlot const &slot, SmallPtrS
 
 auto mlir::atemhir::StoreOp::loadsFrom(MemorySlot const &slot) -> bool
 {
+    return false;
 }
 
 auto mlir::atemhir::StoreOp::storesTo(MemorySlot const &slot) -> bool
 {
+    return this->getAddr() == slot.ptr;
 }
 
 auto mlir::atemhir::StoreOp::getStored(MemorySlot const &slot, OpBuilder &builder, Value reaching_def, DataLayout const &data_layout) -> Value
 {
+    return this->getValue();
 }
 
 auto mlir::atemhir::StoreOp::canUsesBeRemoved(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses,
                                               SmallVectorImpl<OpOperand *> &new_blocking_uses, DataLayout const &data_layout) -> bool
 {
+    if (blocking_uses.size() != 1)
+    {
+        return false;
+    }
+    Value const blocking_use = (*blocking_uses.begin())->get();
+    return blocking_use == slot.ptr and this->getAddr() == slot.ptr and this->getValue().getType() == slot.elemType;
 }
 
 auto mlir::atemhir::StoreOp::removeBlockingUses(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses, OpBuilder &builder,
                                                 Value reaching_def, DataLayout const &data_layout) -> DeletionKind
 {
+    return DeletionKind::Delete;
 }
 
 //========================================================
@@ -156,29 +235,50 @@ auto mlir::atemhir::StoreOp::removeBlockingUses(MemorySlot const &slot, SmallPtr
 
 auto mlir::atemhir::CopyOp::verify() -> LogicalResult
 {
+    if (not this->getType().getPointeeType().hasTrait<DataLayoutTypeInterface::Trait>())
+    {
+        return emitError() << "missing data layout for pointee type";
+    }
 
+    if (this->getSource() == this->getDestination())
+    {
+        return emitError() << "source and destination addresses are the same";
+    }
 }
 
 auto mlir::atemhir::CopyOp::loadsFrom(MemorySlot const &slot) -> bool
 {
+    return this->getSource() == slot.ptr;
 }
 
 auto mlir::atemhir::CopyOp::storesTo(MemorySlot const &slot) -> bool
 {
+    return this->getDestination() == slot.ptr;
 }
 
 auto mlir::atemhir::CopyOp::getStored(MemorySlot const &slot, OpBuilder &builder, Value reaching_def, DataLayout const &data_layout) -> Value
 {
+    return builder.create<LoadOp>(this->getLoc(), slot.elemType, this->getSource());
 }
 
 auto mlir::atemhir::CopyOp::canUsesBeRemoved(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses,
                                              SmallVectorImpl<OpOperand *> &new_blocking_uses, DataLayout const &data_layout) -> bool
 {
+    if (this->getSource() == this->getDestination())
+    {
+        return false;
+    }
+    return this->getLength() == data_layout.getTypeSize(slot.elemType);
 }
 
 auto mlir::atemhir::CopyOp::removeBlockingUses(MemorySlot const &slot, SmallPtrSetImpl<OpOperand *> const &blocking_uses, OpBuilder &builder,
                                                Value reaching_def, DataLayout const &data_layout) -> DeletionKind
 {
+    if (this->loadsFrom(slot))
+    {
+        builder.create<StoreOp>(this->getLoc(), reaching_def, this->getDestination(), false, IntegerAttr{}, MemoryOrderAttr{});
+    }
+    return DeletionKind::Delete;
 }
 
 //========================================================
@@ -187,21 +287,34 @@ auto mlir::atemhir::CopyOp::removeBlockingUses(MemorySlot const &slot, SmallPtrS
 
 auto mlir::atemhir::CastOp::verify() -> LogicalResult
 {
-
+    auto result_type = this->getResult().getType();
+    auto source_type = this->getSource().getType();
 }
 
 auto mlir::atemhir::CastOp::fold(FoldAdaptor adaptor) -> OpFoldResult
 {
-
 }
 
 auto mlir::atemhir::CastOp::canUsesBeRemoved(SmallPtrSetImpl<OpOperand *> const &blocking_uses, SmallVectorImpl<OpOperand *> &new_blocking_uses,
                                              DataLayout const &data_layout) -> bool
 {
+    if (this->getKind() == CastKind::bitcast)
+    {
+        for (Value result : (*this)->getResults())
+        {
+            for (OpOperand &use : result.getUses())
+            {
+                new_blocking_uses.push_back(&use);
+            }
+        }
+        return true;
+    }
+    return false;
 }
 
 auto mlir::atemhir::CastOp::removeBlockingUses(SmallPtrSetImpl<OpOperand *> const &blocking_uses, OpBuilder &builder) -> DeletionKind
 {
+    return DeletionKind::Delete;
 }
 
 //========================================================
@@ -209,6 +322,262 @@ auto mlir::atemhir::CastOp::removeBlockingUses(SmallPtrSetImpl<OpOperand *> cons
 //========================================================
 
 auto mlir::atemhir::IfOp::verify() -> LogicalResult
+{
+    // need to verify types here
+    return success();
+}
+
+auto mlir::atemhir::IfOp::getSuccessorRegions(RegionBranchPoint point, SmallVectorImpl<RegionSuccessor> &successors) -> void
+{
+    if (not point.isParent())
+    {
+        successors.push_back(RegionSuccessor());
+        return;
+    }
+
+    Region *else_region = &this->getElseRegion();
+    if (else_region->empty())
+    {
+        else_region = nullptr;
+    }
+
+    successors.push_back(RegionSuccessor(&this->getThenRegion()));
+    if (else_region)
+    {
+        successors.push_back(RegionSuccessor(else_region));
+    }
+}
+
+auto mlir::atemhir::IfOp::parse(OpAsmParser &parser, OperationState &result) -> ParseResult
+{
+    result.regions.reserve(2);
+    Region *then_region = result.addRegion();
+    Region *else_region = result.addRegion();
+
+    auto &builder = parser.getBuilder();
+
+    OpAsmParser::UnresolvedOperand condition;
+
+    Type bool_type = BoolType::get(builder.getContext());
+
+    if (parser.parseOperand(condition) or parser.resolveOperand(condition, bool_type, result.operands))
+    {
+        return failure();
+    }
+
+    auto parse_then_loc = parser.getCurrentLocation();
+    if (parser.parseRegion(*then_region))
+    {
+        return failure();
+    }
+    if (ensureRegionTerminator(parser, *then_region, parse_then_loc).failed())
+    {
+        return failure();
+    }
+
+    if (not parser.parseOptionalKeyword("else"))
+    {
+        auto parse_else_loc = parser.getCurrentLocation();
+        if (parser.parseRegion(*else_region))
+        {
+            return failure();
+        }
+        if (ensureRegionTerminator(parser, *else_region, parse_else_loc).failed())
+        {
+            return failure();
+        }
+    }
+
+    if (parser.parseColon())
+    {
+        return failure();
+    }
+
+    Type result_type;
+    if (parser.parseType(result_type))
+    {
+        return failure();
+    }
+    result.addTypes(result_type);
+
+    if (parser.parseOptionalAttrDict(result.attributes))
+    {
+        return failure();
+    }
+    return success();
+}
+
+auto mlir::atemhir::IfOp::print(OpAsmPrinter &printer) -> void
+{
+    printer << " " << this->getCondition() << " ";
+    auto &then_region = this->getThenRegion();
+    printer.printRegion(then_region, false, not canOmitRegionTerminator(then_region));
+
+    auto &else_region = this->getElseRegion();
+    if (not else_region.empty())
+    {
+        printer << " else ";
+        printer.printRegion(else_region, false, not canOmitRegionTerminator(else_region));
+    }
+
+    if (this->getResult())
+    {
+        printer << " : " << this->getResult().getType();
+    }
+
+    printer.printOptionalAttrDict(this->getOperation()->getAttrs());
+}
+
+auto mlir::atemhir::IfOp::build(OpBuilder &builder, OperationState &result, Value condition, bool has_else,
+                                function_ref<void(OpBuilder &, Location)> then_builder, function_ref<void(OpBuilder &, Location)> else_builder)
+    -> void
+{
+    assert(then_builder && "the builder for 'then' branch must be present");
+
+    result.addOperands(condition);
+
+    OpBuilder::InsertionGuard guard(builder);
+    Region *then_region = result.addRegion();
+    builder.createBlock(then_region);
+    then_builder(builder, result.location);
+
+    Region *else_region = result.addRegion();
+    if (not has_else)
+    {
+        return;
+    }
+
+    builder.createBlock(else_region);
+    else_builder(builder, result.location);
+}
+
+//========================================================
+// YieldOp Definitions
+//========================================================
+
+auto mlir::atemhir::YieldOp::build(OpBuilder &builder, OperationState &result) -> void
+{
+}
+
+//========================================================
+// BreakOp Definitions
+//========================================================
+
+auto mlir::atemhir::BreakOp::verify() -> LogicalResult
+{
+    if (not this->getOperation()->getParentOfType<AtemHIRLoopOpInterface>())
+    {
+        return emitOpError("'break' must be within a loop");
+    }
+    return success();
+}
+
+//========================================================
+// ContinueOp Definitions
+//========================================================
+
+auto mlir::atemhir::ContinueOp::verify() -> LogicalResult
+{
+    if (not this->getOperation()->getParentOfType<AtemHIRLoopOpInterface>())
+    {
+        return emitOpError("'continue' must be within a loop");
+    }
+    return success();
+}
+
+//========================================================
+// ConditionOp Definitions
+//========================================================
+
+auto mlir::atemhir::ConditionOp::verify() -> LogicalResult
+{
+    if (not mlir::isa<AtemHIRLoopOpInterface>(this->getOperation()->getParentOp()))
+    {
+        return emitOpError("'condition' must be within a loop condition region");
+    }
+    return success();
+}
+
+auto mlir::atemhir::ConditionOp::getSuccessorRegions(ArrayRef<Attribute> operands, SmallVectorImpl<RegionSuccessor> &successors) -> void
+{
+    if (auto loop_op = mlir::dyn_cast<AtemHIRLoopOpInterface>(this->getOperation()->getParentOp()))
+    {
+        successors.emplace_back(&loop_op.getBody(), loop_op.getBody().getArguments());
+        successors.emplace_back(loop_op->getResults());
+    }
+}
+
+auto mlir::atemhir::ConditionOp::getMutableSuccessorOperands(RegionBranchPoint point) -> MutableOperandRange
+{
+    return {this->getOperation(), 0, 0};
+}
+
+//========================================================
+// WhileOp Definitions
+//========================================================
+
+auto mlir::atemhir::WhileOp::getSuccessorRegions(RegionBranchPoint point, SmallVectorImpl<RegionSuccessor> &successors) -> void
+{
+}
+
+auto mlir::atemhir::WhileOp::getLoopRegions() -> SmallVector<Region *>
+{
+}
+
+//========================================================
+// DoWhileOp Definitions
+//========================================================
+
+auto mlir::atemhir::DoWhileOp::getSuccessorRegions(RegionBranchPoint point, SmallVectorImpl<RegionSuccessor> &successors) -> void
+{
+}
+
+auto mlir::atemhir::DoWhileOp::getLoopRegions() -> SmallVector<Region *>
+{
+}
+
+//========================================================
+// ForOp Definitions
+//========================================================
+
+auto mlir::atemhir::ForOp::getSuccessorRegions(RegionBranchPoint point, SmallVectorImpl<RegionSuccessor> &successors) -> void
+{
+}
+
+auto mlir::atemhir::ForOp::getLoopRegions() -> SmallVector<Region *>
+{
+}
+
+//========================================================
+// UnaryOp Definitions
+//========================================================
+
+auto mlir::atemhir::UnaryOp::verify() -> LogicalResult
+{
+}
+
+auto mlir::atemhir::UnaryOp::fold(FoldAdaptor adaptor) -> OpFoldResult
+{
+}
+
+//========================================================
+// BinaryOp Definitions
+//========================================================
+
+auto mlir::atemhir::BinaryOp::verify() -> LogicalResult
+{
+}
+
+//========================================================
+// ScopeOp Definitions
+//========================================================
+
+auto mlir::atemhir::ScopeOp::verify() -> LogicalResult
+{
+
+}
+
+auto mlir::atemhir::ScopeOp::getSuccessorRegions(RegionBranchPoint point, SmallVectorImpl<RegionSuccessor> &successors) -> void
 {
 
 }
--- tests/AtemHIR/IR/function_op_test.mlir
+++ tests/AtemHIR/IR/function_op_test.mlir
@@ -4,9 +4,25 @@
 
 atemhir.function @main() -> !si64 {
     %0 = atemhir.constant #atemhir.int<-42> : !si64
-    %1 = atemhir.constant #atemhir.int<42> : !ui64
+    %1 = atemhir.constant #atemhir.int<42> : !si64
     %2 = atemhir.constant #atemhir.int<42> : !si64
     %3 = atemhir.constant #atemhir.fp<-4.2e3> : !atemhir.fp128
     %4 = atemhir.zeroinit : !atemhir.bool
+    %5 = atemhir.compare lt, %0, %1: !atemhir.int<s, 64>
+
+    atemhir.if %5 {
+        atemhir.yield
+    } 
+    else {
+        atemhir.yield
+    } : !atemhir.unit
+
+    atemhir.while {
+        atemhir.condition %5
+    } 
+    do {
+        %6 = atemhir.allocate_var !atemhir.fp80, !atemhir.ptr<!atemhir.fp80>, ["some_local_var"]
+        atemhir.yield
+    } : !atemhir.unit
     atemhir.return %2 : !si64
 }
